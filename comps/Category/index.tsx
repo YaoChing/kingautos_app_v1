16633905b60d611d3f9252525fb94669c8466a1f
YaoChingYu <yaoching@gamme.com.tw>
2019-11-04 17:46:56 +0800

    將item個別拉出來，讓其他版面使用

--- comps/Category/index.tsx
+++ comps/Category/index.tsx
@@ -1,11 +1,7 @@
 import React, {useState, useEffect, useMemo} from 'react';
 import {
   View,
-  Text,
-  TouchableHighlight,
-  Dimensions,
-  FlatList,
-  Image
+  FlatList
 } from 'react-native';
 import { Spinner } from 'native-base';
 
@@ -14,150 +10,12 @@ import {
 } from '../../libs/ConnectApi';
 import TotalCategories from '../../configs/TotalCategories';
 
-const {height, width} = Dimensions.get('window');
+import ItemCard from './ItemCard';
+
 
 export interface GProps {
   screenProps: {state: any, dispatch: any}
 };
-
-export interface ItemCardProps extends GProps {
-  item: any[],
-  nowIndex: number,
-  totalCount: number,
-  showSpinner: boolean
-}
-export interface ItemCardState {
-  item: any,
-  nowCateName: string,
-  itemWidth: number,
-  smallWidth: number,
-  srcSmallHeight: number,
-  smallScale: number,
-  imgOnLoad: boolean
-}
-
-class _ItemCard extends React.PureComponent <ItemCardProps, ItemCardState> {
-  private _mount: boolean = true;
-
-  constructor(props: ItemCardProps) {
-    super(props);
-
-    this.state = {
-      item: {},
-      nowCateName: '',
-      itemWidth: 0,
-      smallWidth: 0,
-      srcSmallHeight: 0,
-      smallScale: 0,
-      imgOnLoad: true
-    }
-  }
-
-  componentDidMount() {
-    if(this._mount) {
-      let item = this.props.item;
-      let srcWidth = 475;
-      let srcSmallWidth = 475;
-      let srcSmallHeight = 330;
-      let imgScale = 1;
-
-      if(width < srcWidth) {
-        imgScale = (width - 20) / srcWidth;
-      }
-
-      let itemWidth = Math.ceil(srcWidth * imgScale);
-      let smallWidth = itemWidth * 0.4;
-      let smallScale = smallWidth / srcSmallWidth;
-      let nowCateName = '';
-      let nowCateSlug = this.props.screenProps.state.nowCate;
-
-      for(let key in TotalCategories) {
-        let value = TotalCategories[key];
-
-        if(value.slug === nowCateSlug) {
-          nowCateName = value.name;
-        }
-      }
-
-      this.setState({
-        item,
-        nowCateName,
-        itemWidth,
-        smallWidth,
-        srcSmallHeight,
-        smallScale
-      })
-    }
-  }
-
-  getSnapshotBeforeUpdate(prevProps: any, prevState: any) {
-    return null;
-  }
-
-  componentDidUpdate(prevProps: any, prevState: any, snapshot: any) {
-    if (snapshot !== null) {
-      //
-    }
-  }
-
-  componentWillUnmount() {
-    if(this._mount) {
-      return false;
-    }
-  }
-
-  render() {
-    if(Object.keys(this.props.item).length <= 0) return null;
-    if(!this.state.item.imgStr) return null;
-
-    let regData = this.state.item.imgStr.match(/\.jpe?g/);
-    let imgStr = (regData) ? this.state.item.imgStr.replace(regData[0], '-475x330' + regData[0]) : this.state.item.imgStr;
-    let icon = this.state.imgOnLoad ? require('../../files/prepare.jpg') : {uri: imgStr};
-
-    return (
-      <>
-        <View
-          style={{width: this.state.itemWidth, flexDirection: 'row', marginVertical: 5}}>
-          <View
-            style={{flex: 0.8, alignItems: 'center'}}>
-            <Image
-              style={{width: this.state.smallWidth, height: this.state.srcSmallHeight * this.state.smallScale}}
-              source={icon} 
-              onLoadEnd={() => this.setState({imgOnLoad: false})}
-              resizeMode='contain' />
-          </View>
-          <View
-            style={{flex: 1, flexDirection: 'column'}}>
-            <View
-              style={{flex: 0.5, justifyContent: 'center'}}>
-              <Text
-                numberOfLines={1}
-                style={{fontSize: 16}}>{this.state.nowCateName}</Text>
-            </View>
-            <View
-              style={{flex: 1, justifyContent: 'center', marginVertical: 5}}>
-              <Text
-                numberOfLines={2}
-                style={{fontSize: 19}}>{this.state.item.title}</Text>
-            </View>
-            <View
-              style={{flex: 0.5, justifyContent: 'center'}}>
-              <Text
-                numberOfLines={1}
-                style={{fontSize: 16}}>{this.state.item.post_date}</Text>
-            </View>
-          </View>
-        </View>
-        {(this.props.showSpinner && this.props.nowIndex === (this.props.totalCount - 1)) ? (
-          <View
-            style={{width, height: Math.ceil(height * 0.085), justifyContent: 'center', alignItems: 'center'}}>
-            <Spinner color='#b71d29' />
-          </View>
-        ) : null}
-      </>
-    );
-  }
-}
 
 export default (props: GProps) => {
   let [nowPage, setNowPage] = useState(1);
@@ -165,7 +23,6 @@ export default (props: GProps) => {
 
   const _regenData = async (page: number) => {
     let nowCateID = 1;
-    let nowCateName = '';
     let nowCateSlug = props.screenProps.state.nowCate;
 
     for(let key in TotalCategories) {
@@ -173,7 +30,6 @@ export default (props: GProps) => {
 
       if(value.slug === nowCateSlug) {
         nowCateID = value.id;
-        nowCateName = value.name;
       }
     }
 
@@ -231,7 +87,7 @@ export default (props: GProps) => {
         )
       }}
       renderItem={({item, index}) => {
-        return <_ItemCard item={item} nowIndex={index} totalCount={cateData.data.length} showSpinner={nowPage < cateData.totalPage} {...props} />;
+        return <ItemCard item={item} nowIndex={index} totalCount={cateData.data.length} showSpinner={nowPage < cateData.totalPage} {...props} />;
       }}
       />
   );
