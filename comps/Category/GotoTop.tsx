9ae042b141da0d8dc9382c3ae1fc992320c33d5c
YaoChingYu <yaoching@gamme.com.tw>
2019-11-07 16:32:54 +0800

    修改gototop btn位置
    修改gototop btn位置

--- comps/Category/GotoTop.tsx
+++ comps/Category/GotoTop.tsx
@@ -17,7 +17,7 @@ export default (props: GProps) => {
     <TouchableHighlight
       underlayColor={'transparent'}
       onPress={() => props.goToTop()}
-      style={{position: 'absolute', bottom: height * 0.1, right: 30, width: 50, height: 50}} >
+      style={{position: 'absolute', bottom: height * 0.1, right: 5, width: 50, height: 50}} >
       <View
         style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#b71d29', borderColor: '#b71d29', borderWidth: 1, borderRadius: 10, opacity: 0.8}}>
         <Icon name="arrow-with-circle-up" size={40} color="#ffffff" />
