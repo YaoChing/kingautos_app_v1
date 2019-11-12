16b78a1720d352baece7947591414fb39b03d0ad
YaoChingYu <yaoching@gamme.com.tw>
2019-11-12 10:55:01 +0800

    移除監控

--- libs/Analytice.tsx
+++ libs/Analytice.tsx
@@ -2,12 +2,8 @@ import analytics from '@react-native-firebase/analytics';
 
 export const pageView = async (data: {name: string, page: number}) => {
   let result = await analytics().logEvent('PageView', {...data});
-
-  console.log(data, result);
 }
 
 export const pageEvent = async (data: {action: string, name: string}) => {
   let result = await analytics().logEvent('PageEvent', {...data});
-
-  console.log(data, result);
 }
